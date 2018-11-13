9e68eebb3807a355858acfe379ad813eb1673498
yangDaXia <2215890610@qq.com>
2018-11-13 22:18:31 +0800

    更新数据

--- src/router.js
+++ src/router.js
@@ -6,16 +6,31 @@ import Login from "./pages/Login/Login";
 
 import Admin from "./Admin";
 
+// 使用antd 内置语言
+import { LocaleProvider } from 'antd';
+
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+
+// 全局设置 locale 日期插件中文显示
+// import 'moment/locale/zh-cn';
+
+// import moment from 'moment';
+
+// moment.locale('zh-cn');
+
+
 function RouterConfig({ history }) {
   return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/login" exact component={Login} />
-        <Route path="/" render={( props )=>(
-          <Admin/>
-        )} />
-      </Switch>
-    </Router>
+    <LocaleProvider locale={zhCN}>
+      <Router history={history}>
+        <Switch>
+          <Route path="/login" exact component={Login} />
+          <Route path="/" render={( props )=>(
+            <Admin/>
+          )} />
+        </Switch>
+      </Router>
+    </LocaleProvider>
   );
 }
 
