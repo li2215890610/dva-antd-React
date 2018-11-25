9abc6fc36e86889c8624f510c502fd87ec98d7f5
yangDaXia <2215890610@qq.com>
2018-11-25 16:28:07 +0800

    更新

--- src/pages/Ui/Messages/Successs/Successs.js
+++ src/pages/Ui/Messages/Successs/Successs.js
@@ -5,13 +5,13 @@ import { Button, message } from 'antd'
 class Successs extends React.Component {
 
     showMessage = ()=>{
-        message.success("Successs");
+        message.success("Success");
     }
 
     render(){
         return (
             <div>
-              <Button  onClick={this.showMessage}>Successs</Button>
+              <Button  onClick={this.showMessage}>Success</Button>
             </div>
         );
     }
